cd59f8528b8565e24be614ddce228e06ac135c3f
liqiulin <liqiulin@outlook.com>
2023-09-21 15:49:00 +0800

    update: 重构了项目结构

--- hub_test.go
+++ hub_test.go
@@ -3,13 +3,14 @@ package loop
 import (
 	"context"
 	"github.com/stretchr/testify/require"
+	"github.com/weflux/loop/option"
 	"log/slog"
 	"testing"
 )
 
 func Test_NewHub(t *testing.T) {
 
-	hub := NewHub(Options{}, &slog.Logger{})
+	hub := NewHub(option.Options{}, &slog.Logger{})
 	err := hub.Start(context.Background())
 	require.NoError(t, err)
 
